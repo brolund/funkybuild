c2a49144a70ecb7c77c6d6cc2cb6da154b8fbb10
Daniel Brolund <daniel.brolund@agical.com>
2012-03-29 08:05:44 +0200

    Syntax in build.js very much compressed, but not running the main class

--- src/funkybuild.js
+++ src/funkybuild.js
@@ -2,6 +2,9 @@
 	var fb = {};
 	module.exports = fb;
 
+	var async = require('async');
+	var _ = require('underscore');
+
 	var path = require('path');
 	var fn = require('./utils').fn;
 
@@ -16,9 +19,14 @@
 	var Cmd = utils.Cmd;
 	var runCmd = utils.runCmd;
 	
-	var mavenrepo = '/Users/danielbrolund/.m2/repository/'
+	var mavenrepo = '/Users/danielbrolund/.m2/repository/';
 	
+	fb.graph = {};
 	
+	fb.T = function(o, f, i) {
+		fb.graph[o] = _.union(i, f);
+	}
+	var T = fb.T;
 	
  	var runTests = function(testdir, classdirs, libs, cb) {					
 		var tests = _.map(_.filter(walk(testdir), j.isJavaClass), j.toClassName).join(' ');
@@ -29,38 +37,64 @@
 				});
 	}
 
+	fb.build = function() {
+		async.auto(fb.graph,
+		function(err, res) {
+			if(err) {
+				console.log("########## Error");
+				console.trace("Here I am!")
+				console.log(err);
+				console.log(res);
+			} else {
+				console.log("---- Results");
+				console.log(res);
+				/*runCmd(Cmd('java', '.', ['-cp', _.union(res['mainproj.bin'], res['subproj.bin']).join(':'), 'mainprojpackage.Hello']),
+				function(err, exitcode) {
+					console.log(exitcode);
+				});*/
+			}
+		});
+	}	
+	
+	fb.run = function(project, mainClass) {
+		
+	}
+
 	fb.std = function(rootdir, dir, deps) {
 		var nom = function(localdir) {
 			return dir + "." + localdir
 		}
-		var p = {};
-		p[nom('src')]=fn(path.join(rootdir, dir,"/src/main/java/"));
-		p[nom('libs')]=deps&&deps.length>0
-			?_.union(_.map(deps, function(dep) {return dep + '.bin'}), [
-			function(cb, res){
+		T(nom('src'), fn(path.join(rootdir, dir,"/src/main/java/")), []);
+		var dependencies = deps&&deps.length>0
+			?_.map(deps, function(dep) {return dep + '.bin'})
+			:[]
+		var depFunc = deps&&deps.length>0
+			? function(cb, res){
 				var a = _.union(_.map(deps, 
 					function(dep) {
 						return res[dep + '.bin'];
 					}
 				));
-			console.log(a);
-			cb(null, a);
-			}])	
-			:fn([]);
-		p[nom('bin')]=[nom('src'), nom('libs'), function(cb, res) {javac(res[nom('src')],[],res[nom('libs')],cb);}];
-		p[nom('test')]=fn(path.join(rootdir, dir, "/src/test/java/"));
-		p[nom('testlibs')]=fn([mavenrepo + 'junit/junit/4.8.2/junit-4.8.2.jar']);
-		p[nom('testbin')]=[nom('test'), nom('bin'), nom('testlibs'), nom('libs'), 
+				console.log(a);
+				cb(null, a);
+			}
+			: fn([])
+		T(nom('libs'), depFunc, dependencies);
+		T(nom('bin'), function(cb, res) {javac(res[nom('src')],[],res[nom('libs')],cb);}, [nom('src'), nom('libs')]);
+		T(nom('test'), fn(path.join(rootdir, dir, "/src/test/java/")), []);
+		T(nom('testlibs'), fn([mavenrepo + 'junit/junit/4.8.2/junit-4.8.2.jar']), []);
+		
+		T(nom('testbin'),  
 				function(cb, res) {
 					javac(res[nom('test')],[res[nom('bin')]],_.union(res[nom('libs')], res[nom('testlibs')]),cb);
-					}];
-		p[nom('unittestresult')]=[nom('testbin'), nom('bin'), nom('testlibs'), nom('libs'),
+					}, [nom('test'), nom('bin'), nom('testlibs'), nom('libs')]);
+		T(nom('unittestresult'), 
 				function(cb, res) {
 					runTests(res[nom('testbin')], _.union(res[nom('testbin')], res[nom('bin')]),_.union(res[nom('testlibs')], res[nom('libs')]),cb);
-				}];
+				}, [nom('testbin'), nom('bin'), nom('testlibs'), nom('libs')]);
 	
 		console.log("---- Returning standard project object for " + dir);
-		console.log(p);
-		return p;
-	}
+		console.log(fb.graph);
+		return fb.graph;
+	};
 }());
